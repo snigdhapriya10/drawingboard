e08ba88679d4c3266f82bc9872ff5c3cd2823ca0
Snigdha Priya <snigdhapriya2004@gmail.com>
2025-03-25 21:40:09 +0530

    feat:draw triangle

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas"),
-toolBtns=document.querySelectorAll(".tool")
+toolBtns=document.querySelectorAll(".tool"),
+fillColor=document.querySelector("#fill-color"),
 ctx=canvas.getContext("2d");
 //global variables with default value
 let prevMouseX,prevMouseY, snapshot,
@@ -14,8 +15,21 @@ window.addEventListener("load",() => {
 });
 
 const drawRect=(e)=>{
-    ctx.strokeRect(e.offsetX,e.offsetY, prevMouseX - e.offsetX, prevMouseY-e.offsetY);
+    //if fillcolor isnt checked draw a rect with border else draw with bg 
+    if(!fillColor.checked){
+        //creating circle according to mouse pointer
+        return ctx.strokeRect(e.offsetX,e.offsetY, prevMouseX - e.offsetX, prevMouseY-e.offsetY);
+    }
+    ctx.fillRect(e.offsetX,e.offsetY, prevMouseX - e.offsetX, prevMouseY-e.offsetY);
 }
+const drawCircle=(e)=>{
+    ctx.beginPath(); //creating new path to draw circle
+    //getting radius for circle according to the mouse pointer
+    let radius=Math.sqrt(Math.pow((prevMouseX-e.offsetX),2) + Math.pow((prevMouseY-e.offsetY), 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2*Math.PI); //creating circle according to the mouse pointer
+    fillColor.checked? ctx.fill():ctx.stroke(); //if fillcolor is checked fill circle else draw border circle 
+}
+    
 const startDraw =(e)=>{
     isDrawing=true;
     prevMouseX=e.offsetX; //passing current mouseX position as preMouseX value
@@ -30,12 +44,16 @@ const startDraw =(e)=>{
 const drawing = (e) => {
     if (!isDrawing) return; //if isDrawing is false return from here
     ctx.putImageData(snapshot, 0, 0); //adding copied canvas data on to this canvas
+    
     if (selectedTool ==="brush"){ 
     ctx.lineTo(e.offsetX, e.offsetY); //line where mouse pointer goes
     ctx.stroke(); //fills line with colour
     }
     else if(selectedTool === "rectangle"){
         drawRect(e);
+    }
+    else if(selectedTool === "circle"){
+        drawCircle(e);
     }
     
 }
