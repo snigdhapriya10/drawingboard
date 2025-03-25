843f36ada1e5298eae2a6b3c9003ad8d2703ceca
Snigdha Priya <snigdhapriya2004@gmail.com>
2025-03-25 22:52:13 +0530

    feat: size of brush

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("canvas"),
 toolBtns=document.querySelectorAll(".tool"),
 fillColor=document.querySelector("#fill-color"),
+sizeSlider=document.querySelector("#size-slider"),
 ctx=canvas.getContext("2d");
 //global variables with default value
 let prevMouseX,prevMouseY, snapshot,
@@ -78,6 +79,8 @@ toolBtns.forEach(btn => {
         console.log(selectedTool);
     });
 });
+
+sizeSlider.addEventListener("change",()=>brushWidth=sizeSlider.value); //passing slider value as brush size 
 canvas.addEventListener("mousedown",startDraw);
 canvas.addEventListener("mousemove",drawing);
 canvas.addEventListener("mouseup",()=> isDrawing=false);
